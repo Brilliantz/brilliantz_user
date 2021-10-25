340ac268ae45e2479c4593ac098ff4e73f46f43b
wahyupujii <wahyupr952@gmail.com>
2021-10-25 20:05:30 +0700

    preview gambar , border no soal

--- src/pages/In-Exam/index.js
+++ src/pages/In-Exam/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Button, ListGroup } from "react-bootstrap";
+import { Button, ListGroup, Image, Modal } from "react-bootstrap";
 import fire from "../../config/firebase";
 
 const InExam = () => {
@@ -18,6 +18,8 @@ const InExam = () => {
     const [loading, setLoading] = useState(true);
 
     const [dataLocalStorage , setLocalStorage] = useState([])
+
+    const [showModal , setShowModal] = useState(false)
     
     useEffect(() => {
         if (localStorage.key("dataUser")) {
@@ -30,6 +32,7 @@ const InExam = () => {
                 response.forEach(doc => {
                     obj = {
                         ...doc.data(),
+                        isAnswered: false,
                         jawaban: "",
                         beban: 0,
                     }
@@ -37,16 +40,12 @@ const InExam = () => {
                 })
                 temp.sort(compare)
                 setDataSoal(temp)
-                if (localStorage.key("jawaban")) {
-                    // jika di localStorage ada key jawaban , get data nya , yg nantinya berbentuk array
-                    setLocalStorage(JSON.parse(localStorage.getItem("jawaban")))
-                }
-                setLoading(false)
             })
             .catch(err => console.error(err))
+            setLoading(false)
     }, [])
 
-    // fungsi untuk sort array dataSoal
+    // fungsi untuk sort array 
     const compare = (a, b) => {
         if (a.nomor_soal < b.nomor_soal) {
             return -1;
@@ -57,11 +56,12 @@ const InExam = () => {
         return 0;
     }
 
+    // fungsi untuk mengisi jawaban baru / mengganti jawaban
     const choosingAnswer = (answerLetter) => {
-        let array = [...dataLocalStorage]
+        let array = [...dataSoal]
         
         // jika user mengganti jawaban yang sudah pernah dipilih 
-        if (dataSoal[soalTerpilih].jawaban !== "") {
+        if ( dataSoal[soalTerpilih].jawaban !== "" ) {
             dataSoal[soalTerpilih] = {
                 ...dataSoal[soalTerpilih],
                 jawaban: answerLetter
@@ -71,6 +71,7 @@ const InExam = () => {
         } else {
             dataSoal[soalTerpilih] = {
                 ...dataSoal[soalTerpilih],
+                isAnswered: true,
                 jawaban: answerLetter
             }
             // jika user baru saja memilih jawaban , objek dataSoal soal terpilih push ke array
@@ -81,85 +82,71 @@ const InExam = () => {
         localStorage.setItem("jawaban" , JSON.stringify(array.sort(compare)))
     }
 
-    // const kirimJawaban = (e) => {
-    //     e.preventDefault();
-    //     fire.firestore().collection("jawaban").add({
-    //         tryout_id: "8rMpLO1vfy3IzzulsQcq",
-    //         user_id:
-    //     })
-    // }
-
     return (
         <div className="parent-area d-flex container" style={{ position: "absolute", top: "60px", right: "0", bottom: "0", left: "0" }}>
             {
                 loading ? (
                     <span>Loading soal ...</span>
                 ) : (
-                    <>
-                        <div className="soal w-75 p-4 border-right">
-                            <Button variant="primary" disabled={true}>Soal Nomor {dataSoal[soalTerpilih].nomor_soal}</Button>
-                            <p className="py-3 w-75">{dataSoal[soalTerpilih].text_soal}</p>
+                    Object.keys(dataSoal).length !== 0 ? (
+                        <>
+                            <div className="soal w-75 p-4 border-right">
+                                <Button variant="primary" disabled={true}>Soal Nomor {dataSoal[soalTerpilih].nomor_soal}</Button>
+                                <br />
+                                {
+                                    dataSoal[soalTerpilih].hasOwnProperty("gambar_soal") ? (
+                                        <>
+                                            <Image src={dataSoal[soalTerpilih].gambar_soal} className="w-50 my-3" style={{cursor: "pointer"}} fluid onClick={() => setShowModal(true)} />
+                                            <MyVerticallyCenteredModal image={dataSoal[soalTerpilih].gambar_soal} show={showModal} onHide={() => setShowModal(false)} />
+                                        </>
+                                        ) : (<span></span>)
+                                }
 
-                            <div className="pilgan">
-                                <ListGroup>
-                                    <ListGroupItem chooseAnswer={dataSoal[soalTerpilih].jawaban === "A" ? true : false} abjad="A" jawaban={dataSoal[soalTerpilih].a} onClick={() => choosingAnswer("A")} />
-                                    <ListGroupItem chooseAnswer={dataSoal[soalTerpilih].jawaban === "B" ? true : false} abjad="B" jawaban={dataSoal[soalTerpilih].b} onClick={() => choosingAnswer("B")} />
-                                    <ListGroupItem chooseAnswer={dataSoal[soalTerpilih].jawaban === "C" ? true : false} abjad="C" jawaban={dataSoal[soalTerpilih].c} onClick={() => choosingAnswer("C")} />
-                                    <ListGroupItem chooseAnswer={dataSoal[soalTerpilih].jawaban === "D" ? true : false} abjad="D" jawaban={dataSoal[soalTerpilih].d} onClick={() => choosingAnswer("D")} />
-                                    <ListGroupItem chooseAnswer={dataSoal[soalTerpilih].jawaban === "E" ? true : false} abjad="E" jawaban={dataSoal[soalTerpilih].e} onClick={() => choosingAnswer("E")} />
-                                </ListGroup>
+
+                                <p className="py-3 w-75">{dataSoal[soalTerpilih].text_soal}</p>
+
+                                <div className="pilgan">
+                                    <ListGroup>
+                                        <ListGroupItem chooseAnswer={dataSoal[soalTerpilih].jawaban === "A" ? true : false} abjad="A" jawaban={dataSoal[soalTerpilih].a} onClick={() => choosingAnswer("A")} />
+                                        <ListGroupItem chooseAnswer={dataSoal[soalTerpilih].jawaban === "B" ? true : false} abjad="B" jawaban={dataSoal[soalTerpilih].b} onClick={() => choosingAnswer("B")} />
+                                        <ListGroupItem chooseAnswer={dataSoal[soalTerpilih].jawaban === "C" ? true : false} abjad="C" jawaban={dataSoal[soalTerpilih].c} onClick={() => choosingAnswer("C")} />
+                                        <ListGroupItem chooseAnswer={dataSoal[soalTerpilih].jawaban === "D" ? true : false} abjad="D" jawaban={dataSoal[soalTerpilih].d} onClick={() => choosingAnswer("D")} />
+                                        <ListGroupItem chooseAnswer={dataSoal[soalTerpilih].jawaban === "E" ? true : false} abjad="E" jawaban={dataSoal[soalTerpilih].e} onClick={() => choosingAnswer("E")} />
+                                    </ListGroup>
+                                </div>
+
+                                <div className="my-3 pb-4">
+                                    <Button variant="outline-secondary" disabled={soalTerpilih === 0 ? true : false} onClick={() => setSoalTerpilih(soalTerpilih - 1)}>Sebelumnya</Button>
+                                    <Button variant="outline-secondary" disabled={soalTerpilih === dataSoal.length - 1 ? true : false} className="mx-3" onClick={() => setSoalTerpilih(soalTerpilih + 1)}>Selanjutnya</Button>
+                                    <Button variant="success" className={`mx-3 ${soalTerpilih === dataSoal.length - 1 ? "" : "d-none"}`}>Selesai dan Kirim</Button>
+                                </div>
+
                             </div>
 
-                            <div className="mt-3">
-                                <Button variant="outline-secondary" disabled={soalTerpilih === 0 ? true : false} onClick={() => setSoalTerpilih(soalTerpilih - 1)}>Sebelumnya</Button>
-                                <Button variant="outline-secondary" disabled={soalTerpilih === dataSoal.length - 1 ? true : false} className="mx-3" onClick={() => setSoalTerpilih(soalTerpilih + 1)}>Selanjutnya</Button>
-                                <Button variant="success" className="mx-3">Kirim Jawaban</Button>
+                            <div className="nomor-soal w-25 py-4 px-4 border-left">
+                                <ListGroup.Item className="d-flex justify-content-between border">
+                                    <span>Sisa Waktu</span>
+                                    <span>30:00</span>
+                                </ListGroup.Item>
+
+                                <div className="nomor mt-2">
+                                    <table class="table table-borderless">
+                                        <tbody>
+                                            <tr>
+                                                <td className="text-center" onClick={() => setSoalTerpilih(0)} style={{cursor: "pointer" }}><div className={`border ${dataSoal[0].isAnswered === true ? "border-primary" : "border-secondary"} rounded-circle d-flex align-items-center justify-content-center`} style={{ width: "25px", height: "25px", padding: "15px", backgroundColor: soalTerpilih === 0 ? "#FFB332" : ""}} >1</div></td>
+                                                <td className="text-center" onClick={() => setSoalTerpilih(1)} style={{cursor: "pointer" }}><div className={`border ${dataSoal[1].isAnswered === true ? "border-primary" : "border-secondary"} rounded-circle d-flex align-items-center justify-content-center`} style={{ width: "25px", height: "25px", padding: "15px", backgroundColor: soalTerpilih === 1 ? "#FFB332" : ""}} >2</div></td>
+                                                <td className="text-center" onClick={() => setSoalTerpilih(2)} style={{cursor: "pointer" }}><div className={`border ${dataSoal[2].isAnswered === true ? "border-primary" : "border-secondary"} rounded-circle d-flex align-items-center justify-content-center`} style={{ width: "25px", height: "25px", padding: "15px", backgroundColor: soalTerpilih === 2 ? "#FFB332" : ""}} >3</div></td>
+                                                
+                                                <td className="text-center"><div className="border border-secondary rounded-circle d-flex align-items-center justify-content-center" style={{ width: "25px", height: "25px", padding: "15px" }} >4</div></td>
+                                                <td className="text-center"><div className="border border-secondary rounded-circle d-flex align-items-center justify-content-center" style={{ width: "25px", height: "25px", padding: "15px" }} >5</div></td>
+                                            </tr>
+
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
-
-                        </div>
-
-                        <div className="nomor-soal w-25 py-4 px-4 border-left">
-                            <ListGroup.Item className="d-flex justify-content-between border">
-                                <span>Sisa Waktu</span>
-                                <span>30:00</span>
-                            </ListGroup.Item>
-
-                            <div className="nomor mt-2">
-                                <table class="table table-borderless">
-                                    <tbody>
-                                        <tr>
-                                            <td className="text-center" onClick={() => setSoalTerpilih(0)} style={{ cursor: "pointer" }}><div className={`border ${Object.keys(dataLocalStorage).length !== 0 && typeof dataLocalStorage[0] !== 'undefined' ? dataLocalStorage[0].jawaban !== "" ? "border-primary" : "border-secondary" : ""} rounded-circle d-flex align-items-center justify-content-center`} style={{ width: "25px", height: "25px", padding: "15px", backgroundColor: soalTerpilih === 0 ? "#FFB332" : "" }} >1</div></td>
-                                            <td className="text-center" onClick={() => setSoalTerpilih(1)} style={{ cursor: "pointer" }}><div className={`border ${Object.keys(dataLocalStorage).length !== 0 && typeof dataLocalStorage[1] !== 'undefined' ? dataLocalStorage[1].jawaban !== "" ? "border-primary" : "border-secondary" : ""} rounded-circle d-flex align-items-center justify-content-center`} style={{ width: "25px", height: "25px", padding: "15px", backgroundColor: soalTerpilih === 1 ? "#FFB332" : "" }} >2</div></td>
-                                            <td className="text-center" onClick={() => setSoalTerpilih(2)} style={{ cursor: "pointer" }}><div className={`border ${Object.keys(dataLocalStorage).length !== 0 && typeof dataLocalStorage[2] !== 'undefined' ? dataLocalStorage[2].jawaban !== "" ? "border-primary" : "border-secondary" : ""} rounded-circle d-flex align-items-center justify-content-center`} style={{ width: "25px", height: "25px", padding: "15px", backgroundColor: soalTerpilih === 2 ? "#FFB332" : "" }} >3</div></td>
-                                            <td className="text-center"><div className="border border-secondary rounded-circle d-flex align-items-center justify-content-center" style={{ width: "25px", height: "25px", padding: "15px" }} >4</div></td>
-                                            <td className="text-center"><div className="border border-secondary rounded-circle d-flex align-items-center justify-content-center" style={{ width: "25px", height: "25px", padding: "15px" }} >5</div></td>
-                                        </tr>
-                                        <tr>
-                                            <td className="text-center"><div className="border border-secondary rounded-circle d-flex align-items-center justify-content-center" style={{ width: "25px", height: "25px", padding: "15px" }} >6</div></td>
-                                            <td className="text-center"><div className="border border-secondary rounded-circle d-flex align-items-center justify-content-center" style={{ width: "25px", height: "25px", padding: "15px" }} >7</div></td>
-                                            <td className="text-center"><div className="border border-secondary rounded-circle d-flex align-items-center justify-content-center" style={{ width: "25px", height: "25px", padding: "15px" }} >8</div></td>
-                                            <td className="text-center"><div className="border border-secondary rounded-circle d-flex align-items-center justify-content-center" style={{ width: "25px", height: "25px", padding: "15px" }} >9</div></td>
-                                            <td className="text-center"><div className="border border-secondary rounded-circle d-flex align-items-center justify-content-center" style={{ width: "25px", height: "25px", padding: "15px" }} >10</div></td>
-                                        </tr>
-                                        <tr>
-                                            <td className="text-center"><div className="border border-secondary rounded-circle d-flex align-items-center justify-content-center" style={{ width: "25px", height: "25px", padding: "15px" }} >11</div></td>
-                                            <td className="text-center"><div className="border border-secondary rounded-circle d-flex align-items-center justify-content-center" style={{ width: "25px", height: "25px", padding: "15px" }} >12</div></td>
-                                            <td className="text-center"><div className="border border-secondary rounded-circle d-flex align-items-center justify-content-center" style={{ width: "25px", height: "25px", padding: "15px" }} >13</div></td>
-                                            <td className="text-center"><div className="border border-secondary rounded-circle d-flex align-items-center justify-content-center" style={{ width: "25px", height: "25px", padding: "15px" }} >14</div></td>
-                                            <td className="text-center"><div className="border border-secondary rounded-circle d-flex align-items-center justify-content-center" style={{ width: "25px", height: "25px", padding: "15px" }} >15</div></td>
-                                        </tr>
-                                        <tr>
-                                            <td className="text-center"><div className="border border-secondary rounded-circle d-flex align-items-center justify-content-center" style={{ width: "25px", height: "25px", padding: "15px" }} >16</div></td>
-                                            <td className="text-center"><div className="border border-secondary rounded-circle d-flex align-items-center justify-content-center" style={{ width: "25px", height: "25px", padding: "15px" }} >17</div></td>
-                                            <td className="text-center"><div className="border border-secondary rounded-circle d-flex align-items-center justify-content-center" style={{ width: "25px", height: "25px", padding: "15px" }} >18</div></td>
-                                            <td className="text-center"><div className="border border-secondary rounded-circle d-flex align-items-center justify-content-center" style={{ width: "25px", height: "25px", padding: "15px" }} >19</div></td>
-                                            <td className="text-center"><div className="border border-secondary rounded-circle d-flex align-items-center justify-content-center" style={{ width: "25px", height: "25px", padding: "15px" }} >20</div></td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </>
+                        </>
+                    ) : (<span>Loading ... </span>)
                 )
             }
         </div>
@@ -186,6 +173,29 @@ const ListGroupItem = ({ chooseAnswer, abjad, jawaban, ...rest }) => {
             </ListGroup.Item>
         </>
     )
+}
+
+// komponen modal gambar
+const MyVerticallyCenteredModal = (props) => {
+    return (
+        <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            centered
+        >
+            <Modal.Body>
+                <Image src={props.image} fluid />
+            </Modal.Body>
+            <Modal.Footer>
+                <Button onClick={props.onHide}>Close</Button>
+            </Modal.Footer>
+        </Modal>
+    );
+}
+
+const NomorSoal = () => {
+
 }
 
 export default InExam
